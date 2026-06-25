0cb23e8428010dbdc0b81a5d594ca6c5988f504e
xumeng <xumeng.develop@outlook.com>
2026-06-25 11:53:06 +0800

    feat(0205): 同构字符串。

---
+++ src/main/java/com/github/xumeng/problems/easy/problem0205/Solution0205.java
@@ -0,0 +1,27 @@
+package com.github.xumeng.problems.easy.problem0205;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution0205 {
+
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        Map<String, String> characterMap = new HashMap<>();
+        int stringLength = s.length();
+        for (int i = 0; i < stringLength; i++) {
+            String characterFromS = String.valueOf(s.charAt(i));
+            String characterFromT = String.valueOf(t.charAt(i));
+            String targetCharacter = characterMap.get(characterFromS);
+            if (targetCharacter == null) {
+                characterMap.put(characterFromS, characterFromT);
+            } else if (!targetCharacter.equals(characterFromT)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ src/test/java/com/github/xumeng/problems/easy/problem0205/Solution0205Test.java
@@ -0,0 +1,46 @@
+package com.github.xumeng.problems.easy.problem0205;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution0205Test {
+
+    private final Solution0205 solution = new Solution0205();
+
+    @Test
+    public void test1() {
+        String s = "egg";
+        String t = "add";
+        assertTrue(solution.isIsomorphic(s, t));
+    }
+
+    @Test
+    public void test2() {
+        String s = "foo";
+        String t = "bar";
+        assertFalse(solution.isIsomorphic(s, t));
+    }
+
+    @Test
+    public void test3() {
+        String s = "paper";
+        String t = "title";
+        assertTrue(solution.isIsomorphic(s, t));
+    }
+
+    @Test
+    public void test4() {
+        String s = "f11";
+        String t = "b23";
+        assertFalse(solution.isIsomorphic(s, t));
+    }
+
+    @Test
+    public void test5() {
+        String s = "1";
+        String t = "3";
+        assertTrue(solution.isIsomorphic(s, t));
+    }
+
+}
