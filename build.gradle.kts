e79b3bc77c49f7f29899772d4b26d61f6484f934
xumeng <xumeng.develop@outlook.com>
2026-07-02 10:41:54 +0800

    build: 配置 Kotlin 语言的开发环境。

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    kotlin("jvm")
 }
 
 group = "com.github.xumeng-0"
@@ -13,8 +14,12 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
 }
