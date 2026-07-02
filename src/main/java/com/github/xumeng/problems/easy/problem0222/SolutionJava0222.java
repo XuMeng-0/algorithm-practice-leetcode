c9f813647b05e660a6b07445ad22047920b155b0
xumeng <xumeng.develop@outlook.com>
2026-07-02 12:13:05 +0800

    feat(0222): 完全二叉树的节点个数，采用递归方案计算节点个数。

--- src/main/java/com/github/xumeng/problems/easy/problem0222/SolutionJava0222.java
+++ src/main/java/com/github/xumeng/problems/easy/problem0222/SolutionJava0222.java
@@ -5,7 +5,18 @@ import com.github.xumeng.common.datastructure.TreeNode;
 public class SolutionJava0222 {
 
     public int countNodes(TreeNode root) {
-        return 0;
+        int count = 0;
+        if (root == null) {
+            return count;
+        }
+        count = 1;
+        if (root.left != null) {
+            count += countNodes(root.left);
+        }
+        if (root.right != null) {
+            count += countNodes(root.right);
+        }
+        return count;
     }
 
 }
