3bb181e6af8906d6b6096f339abee3b85323f3d3
xumeng <xumeng.develop@outlook.com>
2026-07-02 10:44:00 +0800

    feat(0219): 存在重复元素 II , 使用 Kotlin 解题。

---
+++ src/main/kotlin/com/github/xumeng/problems/easy/problem0219/SolutionKotlin0219.kt
@@ -0,0 +1,18 @@
+package com.github.xumeng.problems.easy.problem0219
+
+class SolutionKotlin0219 {
+
+    fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
+        val numsLength: Int = nums.size
+        for (i in 0 until numsLength) {
+            val max: Int = (i + k).coerceAtMost(numsLength - 1)
+            for (j in i + 1 until max + 1) {
+                if (nums[i] == nums[j]) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+}
---
+++ src/test/kotlin/com/github/xumeng/problems/easy/problem0219/SolutionKotlin0219Test.kt
@@ -0,0 +1,51 @@
+package com.github.xumeng.problems.easy.problem0219
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class SolutionKotlin0219Test {
+
+    private val solution: SolutionKotlin0219 = SolutionKotlin0219()
+
+    @Test
+    fun test1() {
+        val nums: IntArray = intArrayOf(1, 2, 3, 1)
+        val k = 3
+        assertTrue { solution.containsNearbyDuplicate(nums, k) }
+    }
+
+    @Test
+    fun test2() {
+        val nums: IntArray = intArrayOf(1, 0, 1, 1)
+        val k = 1
+        assertTrue { solution.containsNearbyDuplicate(nums, k) }
+    }
+
+    @Test
+    fun test3() {
+        val nums: IntArray = intArrayOf(1, 2, 3, 1, 2, 3)
+        val k = 2
+        assertFalse { solution.containsNearbyDuplicate(nums, k) }
+    }
+
+    @Test
+    fun test4() {
+        val nums: IntArray = intArrayOf(1)
+        val k = 0
+        assertFalse { solution.containsNearbyDuplicate(nums, k) }
+    }
+
+    @Test
+    fun test5() {
+        val nums: IntArray = intArrayOf(1, 1)
+        val k = 0
+        assertFalse { solution.containsNearbyDuplicate(nums, k) }
+    }
+
+    @Test
+    fun test6() {
+        val nums: IntArray = intArrayOf(1, 2, 0, 0, 3, 1)
+        val k = 4
+        assertTrue { solution.containsNearbyDuplicate(nums, k) }
+    }
+}
