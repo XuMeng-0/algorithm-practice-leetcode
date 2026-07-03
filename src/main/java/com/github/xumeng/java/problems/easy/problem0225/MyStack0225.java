e0a46644802a595e9767317b4df73b045ca8dcc0
xumeng <xumeng.develop@outlook.com>
2026-07-03 18:23:05 +0800

    feat(0225): 用队列实现栈。

--- src/main/java/com/github/xumeng/java/problems/easy/problem0225/MyStack0225.java
+++ src/main/java/com/github/xumeng/java/problems/easy/problem0225/MyStack0225.java
@@ -1,25 +1,50 @@
 package com.github.xumeng.java.problems.easy.problem0225;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class MyStack0225 {
+
+    private ConcurrentLinkedQueue<Integer> queue1 = new ConcurrentLinkedQueue<>();
+    private ConcurrentLinkedQueue<Integer> queue2 = new ConcurrentLinkedQueue<>();
 
     public MyStack0225() {
 
     }
 
     public void push(int x) {
-
+        queue1.offer(x);
     }
 
     public int pop() {
-        return 0;
+        while (!queue1.isEmpty()) {
+            Integer element = queue1.poll();
+            if (queue1.isEmpty()) {
+                ConcurrentLinkedQueue<Integer> queue = queue2;
+                queue2 = queue1;
+                queue1 = queue;
+                return element;
+            }
+            queue2.offer(element);
+        }
+        return -1;
     }
 
     public int top() {
-        return 0;
+        while (!queue1.isEmpty()) {
+            Integer element = queue1.poll();
+            queue2.offer(element);
+            if (queue1.isEmpty()) {
+                ConcurrentLinkedQueue<Integer> queue = queue2;
+                queue2 = queue1;
+                queue1 = queue;
+                return element;
+            }
+        }
+        return -1;
     }
 
     public boolean empty() {
-        return true;
+        return queue1.isEmpty();
     }
 
 }
