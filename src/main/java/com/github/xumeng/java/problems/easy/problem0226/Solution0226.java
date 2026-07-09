298ddbb259870573701226a5f7382ef4ae15175b
xumeng <xumeng.develop@outlook.com>
2026-07-09 19:04:27 +0800

    feat(0226): 翻转二叉树，使用递归方案翻转。

--- src/main/java/com/github/xumeng/java/problems/easy/problem0226/Solution0226.java
+++ src/main/java/com/github/xumeng/java/problems/easy/problem0226/Solution0226.java
@@ -5,6 +5,18 @@ import com.github.xumeng.java.common.datastructure.TreeNode;
 public class Solution0226 {
 
     public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.left != null) {
+            invertTree(root.left);
+        }
+        if (root.right != null) {
+            invertTree(root.right);
+        }
+        TreeNode node = root.left;
+        root.left = root.right;
+        root.right = node;
         return root;
     }
 
