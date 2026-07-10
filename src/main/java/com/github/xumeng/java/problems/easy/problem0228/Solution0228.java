1ed995f56b4d0add3980eb6baf8a92ea3fd5f8d4
xumeng <xumeng.develop@outlook.com>
2026-07-10 18:12:59 +0800

    feat(0228): 汇总区间，Java 。

--- src/main/java/com/github/xumeng/java/problems/easy/problem0228/Solution0228.java
+++ src/main/java/com/github/xumeng/java/problems/easy/problem0228/Solution0228.java
@@ -6,7 +6,25 @@ import java.util.List;
 public class Solution0228 {
 
     public List<String> summaryRanges(int[] nums) {
-        return new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        if (nums.length == 0) {
+            return result;
+        }
+        int start = 0;
+        int end = start;
+        while (end + 1 < nums.length) {
+            if (nums[end] + 1 == nums[end + 1]) {
+                end++;
+            } else {
+                String element = start == end ? String.valueOf(nums[start]) : nums[start] + "->" + nums[end];
+                result.add(element);
+                start = end + 1;
+                end = start;
+            }
+        }
+        String element = start == end ? String.valueOf(nums[start]) : nums[start] + "->" + nums[end];
+        result.add(element);
+        return result;
     }
 
 }
