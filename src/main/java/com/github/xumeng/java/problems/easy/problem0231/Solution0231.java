e14c9eb86192a25e3082bb1d04dd5c474d03b9a8
xumeng <xumeng.develop@outlook.com>
2026-07-16 11:11:44 +0800

    feat(0231): 2 的幂，添加问题描述，添加测试用例。

---
+++ src/main/java/com/github/xumeng/java/problems/easy/problem0231/Solution0231.java
@@ -0,0 +1,9 @@
+package com.github.xumeng.java.problems.easy.problem0231;
+
+public class Solution0231 {
+
+    public boolean isPowerOfTwo(int n) {
+        return true;
+    }
+
+}
---
+++ src/test/java/com/github/xumeng/java/problems/easy/problem0231/Solution0231Test.java
@@ -0,0 +1,46 @@
+package com.github.xumeng.java.problems.easy.problem0231;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution0231Test {
+
+    private final Solution0231 solution = new Solution0231();
+
+    @Test
+    public void test1() {
+        int n = 1;
+        boolean result = solution.isPowerOfTwo(n);
+        assertTrue(result);
+    }
+
+    @Test
+    public void test2() {
+        int n = 3;
+        boolean result = solution.isPowerOfTwo(n);
+        assertFalse(result);
+    }
+
+    @Test
+    public void test3() {
+        int n = 16;
+        boolean result = solution.isPowerOfTwo(n);
+        assertTrue(result);
+    }
+
+    @Test
+    public void test4() {
+        int n = 0;
+        boolean result = solution.isPowerOfTwo(n);
+        assertFalse(result);
+    }
+
+    @Test
+    public void test5() {
+        int n = -1;
+        boolean result = solution.isPowerOfTwo(n);
+        assertFalse(result);
+    }
+
+}
