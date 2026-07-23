efa3ba89518ac3d6cac2f4e59c1efa79ed45033b
xumeng <xumeng.develop@outlook.com>
2026-07-23 11:22:10 +0800

    feat(datastructure): 定义链表节点类，Java 。

---
+++ src/main/java/com/github/xumeng/java/common/datastructure/ListNode.java
@@ -0,0 +1,22 @@
+package com.github.xumeng.java.common.datastructure;
+
+public class ListNode {
+
+    public int value;
+
+    public ListNode next;
+
+    public ListNode() {
+
+    }
+
+    public ListNode(int value) {
+        this.value = value;
+    }
+
+    public ListNode(int value, ListNode next) {
+        this.value = value;
+        this.next = next;
+    }
+
+}
