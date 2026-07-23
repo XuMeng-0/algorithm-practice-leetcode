1719cb8d38094d0f2607479e15259fbada2bb666
xumeng <xumeng.develop@outlook.com>
2026-07-23 19:13:41 +0800

    feat(0234): 回文链表，解决问题，Kotlin 。

--- src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0234/Solution0234.kt
+++ src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0234/Solution0234.kt
@@ -5,7 +5,26 @@ import com.github.xumeng.kotlin.common.datastructure.ListNode
 class Solution0234 {
 
     fun isPalindrome(head: ListNode?): Boolean {
-        return false
+        val elementList = mutableListOf<Int>()
+        var node = head
+        do {
+            if (node != null) {
+                elementList.add(node.value)
+                node = node.next
+            }
+        } while (node != null)
+        var i = 0
+        var j = elementList.size - 1
+        while (i <= j) {
+            val e1: Int = elementList.get(i)
+            val e2: Int = elementList.get(j)
+            if (e1 != e2) {
+                return false
+            }
+            i++
+            j--
+        }
+        return true
     }
 
 }
