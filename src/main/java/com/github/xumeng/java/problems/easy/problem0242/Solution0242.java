805c0ca381fda0f0922b64b6ed46d7ce233c89d3
xumeng <xumeng.develop@outlook.com>
2026-07-24 11:38:18 +0800

    feat(0242): 字母异位词，解决问题，Java 。

--- src/main/java/com/github/xumeng/java/problems/easy/problem0242/Solution0242.java
+++ src/main/java/com/github/xumeng/java/problems/easy/problem0242/Solution0242.java
@@ -1,9 +1,48 @@
 package com.github.xumeng.java.problems.easy.problem0242;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
 public class Solution0242 {
 
     public boolean isAnagram(String s, String t) {
-        return false;
+        if (s.length() != t.length()) {
+            return false;
+        }
+        Map<Character, Integer> charactersOfS = countCharacters(s);
+        Map<Character, Integer> charactersOfT = countCharacters(t);
+        int size1 = charactersOfS.size();
+        int size2 = charactersOfT.size();
+        if (size1 != size2) {
+            return false;
+        }
+        Set<Character> characters = charactersOfS.keySet();
+        for (Character character : characters) {
+            Integer countS = charactersOfS.get(character);
+            Integer countT = charactersOfT.get(character);
+            if (!Objects.equals(countS, countT)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private Map<Character, Integer> countCharacters(String s) {
+        Map<Character, Integer> charactersMap = new HashMap<>();
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char character = s.charAt(i);
+            Integer count = charactersMap.get(character);
+            if (count != null) {
+                count++;
+            } else {
+                count = 1;
+            }
+            charactersMap.put(character, count);
+        }
+        return charactersMap;
     }
 
 }
