0a9b441b9bcceb09eb1d936542565b14b31e66b1
xumeng <xumeng.develop@outlook.com>
2026-07-30 18:26:52 +0800

    feat(0257): 二叉树的所有路径，解决问题，Kotlin 。

--- src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0257/Solution0257.kt
+++ src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0257/Solution0257.kt
@@ -5,7 +5,26 @@ import com.github.xumeng.kotlin.common.datastructure.TreeNode
 class Solution0257 {
 
     fun binaryTreePaths(root: TreeNode?): List<String> {
-        return emptyList()
+        val paths: MutableList<String> = mutableListOf()
+        if (root == null) {
+            return paths
+        }
+        if (root.left == null && root.right == null) {
+            paths.add(root.value.toString())
+        }
+        if (root.left != null) {
+            val pathsOfLeft: List<String> = binaryTreePaths(root.left)
+            for (path: String in pathsOfLeft) {
+                paths.add(root.value.toString() + "->" + path)
+            }
+        }
+        if (root.right != null) {
+            val pathsOfRight: List<String> = binaryTreePaths(root.right)
+            for (path: String in pathsOfRight) {
+                paths.add(root.value.toString() + "->" + path)
+            }
+        }
+        return paths
     }
 
 }
