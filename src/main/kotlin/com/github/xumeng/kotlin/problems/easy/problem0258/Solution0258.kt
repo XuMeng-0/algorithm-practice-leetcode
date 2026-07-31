2df4b86cc5df1521fc8781fc0261fc7d6c835376
xumeng <xumeng.develop@outlook.com>
2026-07-31 11:42:10 +0800

    feat(0258): 各位相加，解决问题，Kotlin 。

--- src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0258/Solution0258.kt
+++ src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0258/Solution0258.kt
@@ -1,9 +1,29 @@
 package com.github.xumeng.kotlin.problems.easy.problem0258
 
+import kotlin.math.pow
+
 class Solution0258 {
 
     fun addDigits(num: Int): Int {
-        return 1
+        if (num < 10) {
+            return num
+        }
+        var number = num
+        var index = 9
+        var divisor: Int
+        var quotient: Byte
+        var sum = 0
+        while (index >= 0) {
+            divisor = 10.0.pow(index).toInt()
+            quotient = (number / divisor).toByte()
+            sum += quotient
+            number %= divisor
+            index--
+        }
+        while (sum > 9) {
+            sum = addDigits(sum)
+        }
+        return sum
     }
 
 }
