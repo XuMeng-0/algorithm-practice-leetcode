b81edaf91d30e133f7b82c3e73fc93c071b0b92d
xumeng <xumeng.develop@outlook.com>
2026-07-31 18:55:31 +0800

    feat(0258): 各位相加，解决问题，Java 。

--- src/main/java/com/github/xumeng/java/problems/easy/problem0258/Solution0258.java
+++ src/main/java/com/github/xumeng/java/problems/easy/problem0258/Solution0258.java
@@ -3,7 +3,19 @@ package com.github.xumeng.java.problems.easy.problem0258;
 public class Solution0258 {
 
     public int addDigits(int num) {
-        return 1;
+        if (num < 10) {
+            return num;
+        }
+        int sum = 0;
+        for (int i = 9, divisor; i >= 0; i--) {
+            divisor = (int) Math.pow(10.0, i);
+            sum += (num / divisor);
+            num %= divisor;
+        }
+        while (sum > 9) {
+            sum = addDigits(sum);
+        }
+        return sum;
     }
 
 }
