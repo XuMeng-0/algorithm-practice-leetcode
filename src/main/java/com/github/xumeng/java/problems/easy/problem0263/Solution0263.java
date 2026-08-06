0ff6bdcec7c02588cfe6b9596fb887d519988494
xumeng <xumeng.develop@outlook.com>
2026-08-06 16:21:41 +0800

    feat(0263): 丑数，解决问题，Java 。

--- src/main/java/com/github/xumeng/java/problems/easy/problem0263/Solution0263.java
+++ src/main/java/com/github/xumeng/java/problems/easy/problem0263/Solution0263.java
@@ -1,9 +1,51 @@
 package com.github.xumeng.java.problems.easy.problem0263;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution0263 {
 
     public boolean isUgly(int n) {
-        return false;
+        if (n <= 0) {
+            return false;
+        }
+        List<Integer> factors = findPrimeFactors(n);
+        for (Integer i : factors) {
+            if (i > 5) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private List<Integer> findPrimeFactors(int n) {
+        List<Integer> factors = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            int quotient = n / i;
+            int remainder = n % i;
+            if (quotient < i) {
+                break;
+            }
+            if (remainder != 0) {
+                continue;
+            }
+            if (!factors.contains(i) && isPrime(i)) {
+                factors.add(i);
+            }
+            if (!factors.contains(quotient) && isPrime(quotient)) {
+                factors.add(quotient);
+            }
+        }
+        return factors;
+    }
+
+    private boolean isPrime(int n) {
+        for (int i = 2; i < n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
