fb9ad2a390091fb1ae5ce045c4a8fad9a180aa0f
xumeng <xumeng.develop@outlook.com>
2026-08-06 17:14:05 +0800

    feat(0268): 丢失的数字，解决问题，Java 。

--- src/main/java/com/github/xumeng/java/problems/easy/problem0268/Solution0268.java
+++ src/main/java/com/github/xumeng/java/problems/easy/problem0268/Solution0268.java
@@ -1,9 +1,20 @@
 package com.github.xumeng.java.problems.easy.problem0268;
 
+import java.util.Arrays;
+
 public class Solution0268 {
 
     public int missingNumber(int[] nums) {
-        return 1;
+        Arrays.sort(nums);
+        if (nums[0] != 0) {
+            return 0;
+        }
+        for (int i = 0; i + 1 < nums.length; i++) {
+            if (nums[i + 1] - nums[i] != 1) {
+                return nums[i] + 1;
+            }
+        }
+        return nums.length;
     }
 
 }
