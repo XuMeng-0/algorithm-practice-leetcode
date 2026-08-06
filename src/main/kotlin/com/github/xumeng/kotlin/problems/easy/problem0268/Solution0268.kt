ad70c273ae4e2e1191a2968f59219cacf17e803c
xumeng <xumeng.develop@outlook.com>
2026-08-06 17:30:20 +0800

    feat(0268): 丢失的数字，解决问题，Kotlin 。

--- src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0268/Solution0268.kt
+++ src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0268/Solution0268.kt
@@ -3,7 +3,16 @@ package com.github.xumeng.kotlin.problems.easy.problem0268
 class Solution0268 {
 
     fun missingNumber(nums: IntArray): Int {
-        return 1
+        nums.sort()
+        if (nums[0] != 0) {
+            return 0
+        }
+        for (i in 0..<nums.size - 1) {
+            if (nums[i + 1] - nums[i] != 1) {
+                return nums[i] + 1
+            }
+        }
+        return nums.size
     }
 
 }
