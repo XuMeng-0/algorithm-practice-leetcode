7ea51f86019760ddff06f5390be94ffabe32c426
xumeng <xumeng.develop@outlook.com>
2026-08-07 18:34:03 +0800

    feat(0263): 丑数，解决问题，Kotlin 。

--- src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0263/Solution0263.kt
+++ src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0263/Solution0263.kt
@@ -3,7 +3,37 @@ package com.github.xumeng.kotlin.problems.easy.problem0263
 class Solution0263 {
 
     fun isUgly(n: Int): Boolean {
-        return false
+        if (n <= 0) {
+            return false
+        }
+        var quotient: Int = n
+        var i = 1
+        while (i <= quotient) {
+            quotient = n / i
+            if (n % i != 0) {
+                i++
+                continue
+            }
+            if (isPrime(i) && i > 5) {
+                return false
+            }
+            if (isPrime(quotient) && quotient > 5) {
+                return false
+            }
+            i++
+        }
+        return true
+    }
+
+    private fun isPrime(n: Int): Boolean {
+        var i = 2
+        while (i <= n / i) {
+            if (n % i == 0) {
+                return false
+            }
+            i++
+        }
+        return true
     }
 
 }
