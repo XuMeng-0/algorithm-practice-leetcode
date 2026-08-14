998261d1e1a9d25ca1182372d00255411a1c63cc
xumeng <xumeng.develop@outlook.com>
2026-08-14 18:34:34 +0800

    feat(0283): 移动零，解决问题，方案一，Kotlin 。

--- src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0283/Solution0283S1.kt
+++ src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0283/Solution0283S1.kt
@@ -2,8 +2,22 @@ package com.github.xumeng.kotlin.problems.easy.problem0283
 
 class Solution0283S1 {
 
-    fun moveZeroes(nums: IntArray): Unit {
-
+    fun moveZeroes(nums: IntArray) {
+        val length: Int = nums.size
+        var temp: Int
+        for (i in 0..<length) {
+            for (j in i + 1..<length) {
+                if (nums[i] != 0) {
+                    continue
+                }
+                if (nums[j] == 0) {
+                    continue
+                }
+                temp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = temp
+            }
+        }
     }
 
 }
