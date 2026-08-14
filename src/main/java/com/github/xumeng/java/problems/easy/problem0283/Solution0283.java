29d05f24143b2d9d166842557ab331cb6fb02055
xumeng <xumeng.develop@outlook.com>
2026-08-14 11:16:32 +0800

    feat(0283): 移动零，解决问题，Java 。

--- src/main/java/com/github/xumeng/java/problems/easy/problem0283/Solution0283.java
+++ src/main/java/com/github/xumeng/java/problems/easy/problem0283/Solution0283.java
@@ -3,7 +3,20 @@ package com.github.xumeng.java.problems.easy.problem0283;
 public class Solution0283 {
 
     public void moveZeroes(int[] nums) {
-
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (nums[i] != 0) {
+                    continue;
+                }
+                if (nums[j] == 0) {
+                    continue;
+                }
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
     }
 
 }
