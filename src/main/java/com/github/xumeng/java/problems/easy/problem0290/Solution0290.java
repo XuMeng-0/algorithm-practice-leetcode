2d401360038386fc38effe4ab9d868598c52544e
xumeng <xumeng.develop@outlook.com>
2026-08-20 20:19:38 +0800

    feat(0290): 单词规律，解决问题，Java 。

--- src/main/java/com/github/xumeng/java/problems/easy/problem0290/Solution0290.java
+++ src/main/java/com/github/xumeng/java/problems/easy/problem0290/Solution0290.java
@@ -1,9 +1,38 @@
 package com.github.xumeng.java.problems.easy.problem0290;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Solution0290 {
 
     public boolean wordPattern(String pattern, String s) {
-        return false;
+        String[] stringElements = s.split(" ");
+        String[] patternElements = pattern.split("");
+        if (stringElements.length != patternElements.length) {
+            return false;
+        }
+        Map<String, String> map = new HashMap<>();
+        List<String> mapValueElements = new ArrayList<>();
+        for (int i = 0; i < stringElements.length; i++) {
+            String patternElement = patternElements[i];
+            String stringElement = stringElements[i];
+            String target = map.get(patternElement);
+            if (target == null) {
+                if (mapValueElements.contains(stringElement)) {
+                    return false;
+                } else {
+                    map.put(patternElement, stringElement);
+                    mapValueElements.add(stringElement);
+                }
+            } else {
+                if (!target.equals(stringElement)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 }
