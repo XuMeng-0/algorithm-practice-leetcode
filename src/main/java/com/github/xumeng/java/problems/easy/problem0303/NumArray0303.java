591fde34a32d1c62642c43f551f886a0fb3e3ebc
xumeng <xumeng.develop@outlook.com>
2026-08-27 18:46:05 +0800

    feat(0303): 区域和检索，解决问题，Java 。

--- src/main/java/com/github/xumeng/java/problems/easy/problem0303/NumArray0303.java
+++ src/main/java/com/github/xumeng/java/problems/easy/problem0303/NumArray0303.java
@@ -2,13 +2,19 @@ package com.github.xumeng.java.problems.easy.problem0303;
 
 public class NumArray0303 {
 
+    private final int[] nums;
+
 
     public NumArray0303(int[] nums) {
-
+        this.nums = nums;
     }
 
     public int sumRange(int left, int right) {
-        return 0;
+        int sum = 0;
+        for (int i = left; i <= right; i++) {
+            sum += nums[i];
+        }
+        return sum;
     }
 
 }
