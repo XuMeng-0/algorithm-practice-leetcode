8ebcc9a9ca5a410b9d22507fe50d29c92ae51b62
xumeng <xumeng.develop@outlook.com>
2026-08-27 19:10:01 +0800

    feat(0303): 区域和检索，解决问题，Kotlin 。

--- src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0303/NumArray0303.kt
+++ src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0303/NumArray0303.kt
@@ -2,8 +2,14 @@ package com.github.xumeng.kotlin.problems.easy.problem0303
 
 class NumArray0303(nums: IntArray) {
 
+    private var numArray = nums
+
     fun sumRange(left: Int, right: Int): Int {
-        return 0
+        var sum = 0
+        for (i in left..right) {
+            sum += numArray[i]
+        }
+        return sum
     }
 
 }
