034a0ce33f2e9979a2c6bd322fdff9dc2c94f5a3
xumeng <xumeng.develop@outlook.com>
2026-08-28 11:13:32 +0800

    feat(0326): 3 的幂，解决问题，Kotlin 。

--- src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0326/Solution0326.kt
+++ src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0326/Solution0326.kt
@@ -1,8 +1,26 @@
 package com.github.xumeng.kotlin.problems.easy.problem0326
 
+import kotlin.math.pow
+
 class Solution0326 {
 
     fun isPowerOfThree(n: Int): Boolean {
+        if (n < 1) {
+            return false
+        }
+        val base = 3.0
+        var i = 0
+        var power = base.pow(i)
+        while (power <= Int.MAX_VALUE) {
+            if (n == power.toInt()) {
+                return true
+            }
+            if (n < power) {
+                return false
+            }
+            i++
+            power = base.pow(i)
+        }
         return false
     }
 
