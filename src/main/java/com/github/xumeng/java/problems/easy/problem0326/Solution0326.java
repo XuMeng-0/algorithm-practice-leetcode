7e4dca3056a0806ea245a3220f5f3c680fff6e61
xumeng <xumeng.develop@outlook.com>
2026-08-28 18:08:07 +0800

    feat(0326): 3 的幂，解决问题，Java 。

--- src/main/java/com/github/xumeng/java/problems/easy/problem0326/Solution0326.java
+++ src/main/java/com/github/xumeng/java/problems/easy/problem0326/Solution0326.java
@@ -3,6 +3,20 @@ package com.github.xumeng.java.problems.easy.problem0326;
 public class Solution0326 {
 
     public boolean isPowerOfThree(int n) {
+        if (n < 1) {
+            return false;
+        }
+        int base = 3;
+        double power = 0.0;
+        for (int i = 0; power <= Integer.MAX_VALUE; i++) {
+            power = Math.pow(base, i);
+            if (n == power) {
+                return true;
+            }
+            if (n < power) {
+                return false;
+            }
+        }
         return false;
     }
 
