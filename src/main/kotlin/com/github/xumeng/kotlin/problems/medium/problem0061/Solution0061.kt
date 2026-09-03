f86d784555fa58b5f9475dbba5dfd921e22f2572
xumeng <xumeng.develop@outlook.com>
2026-09-03 20:30:15 +0800

    feat(0061): 旋转链表，解决问题，Kotlin 。

--- src/main/kotlin/com/github/xumeng/kotlin/problems/medium/problem0061/Solution0061.kt
+++ src/main/kotlin/com/github/xumeng/kotlin/problems/medium/problem0061/Solution0061.kt
@@ -5,7 +5,48 @@ import com.github.xumeng.kotlin.common.datastructure.ListNode
 class Solution0061 {
 
     fun rotateRight(head: ListNode?, k: Int): ListNode? {
-        return head
+        if (head == null) {
+            return null
+        }
+        val listLength = calculateListLength(head)
+        var i = k % listLength
+        var newHead: ListNode = head
+        while (i > 0) {
+            newHead = moveLastToFirst(newHead)
+            i--
+        }
+        return newHead
+    }
+
+    private fun calculateListLength(head: ListNode?): Int {
+        var node = head
+        var length = 0
+        while (node != null) {
+            length++
+            node = node.next
+        }
+        return length
+    }
+
+    private fun moveLastToFirst(head: ListNode): ListNode {
+        if (head.next == null) {
+            return head
+        }
+        var lastNode: ListNode? = null
+        var node: ListNode? = head
+        while (node != null) {
+            val nextNode = node.next
+            if (nextNode != null && nextNode.next == null) {
+                lastNode = node.next
+                node.next = null
+            }
+            node = node.next
+        }
+        if (lastNode == null) {
+            return head
+        }
+        lastNode.next = head
+        return lastNode
     }
 
 }
