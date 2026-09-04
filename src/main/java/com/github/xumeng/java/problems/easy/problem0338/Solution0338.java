3be12928759eaef2eb8b04a678be28f87c4a6ef0
xumeng <xumeng.develop@outlook.com>
2026-09-04 18:46:04 +0800

    feat(0338): 比特位计数，解决问题，Java 。

--- src/main/java/com/github/xumeng/java/problems/easy/problem0338/Solution0338.java
+++ src/main/java/com/github/xumeng/java/problems/easy/problem0338/Solution0338.java
@@ -3,7 +3,25 @@ package com.github.xumeng.java.problems.easy.problem0338;
 public class Solution0338 {
 
     public int[] countBits(int n) {
-        return new int[0];
+        int arrayLength = n + 1;
+        int[] result = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            result[i] = countNumberOfOne(i);
+        }
+        return result;
+    }
+
+    private int countNumberOfOne(int n) {
+        int quotient;
+        int countOfOne = 0;
+        while (n != 0) {
+            quotient = n / 2;
+            if (n % 2 == 1) {
+                countOfOne++;
+            }
+            n = quotient;
+        }
+        return countOfOne;
     }
 
 }
