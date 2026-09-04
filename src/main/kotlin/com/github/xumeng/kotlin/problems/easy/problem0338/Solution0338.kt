a720bf957a08083e936956d6ffc37a7b25ab5bcc
xumeng <xumeng.develop@outlook.com>
2026-09-04 18:32:04 +0800

    feat(0338): 比特位计数，解决问题，Kotlin 。

--- src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0338/Solution0338.kt
+++ src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0338/Solution0338.kt
@@ -3,7 +3,25 @@ package com.github.xumeng.kotlin.problems.easy.problem0338
 class Solution0338 {
 
     fun countBits(n: Int): IntArray {
-        return intArrayOf()
+        val result = IntArray(n + 1)
+        for (i in 0..n) {
+            result[i] = countNumberOfOne(i)
+        }
+        return result
+    }
+
+    private fun countNumberOfOne(n: Int): Int {
+        var dividend: Int = n
+        var quotient: Int
+        var countOfOne = 0
+        while (dividend != 0) {
+            quotient = dividend / 2
+            if (dividend % 2 == 1) {
+                countOfOne++
+            }
+            dividend = quotient
+        }
+        return countOfOne
     }
 
 }
