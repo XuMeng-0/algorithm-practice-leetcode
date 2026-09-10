bb1975229bdfe700afa1c2b21341e52c7294de8a
xumeng <xumeng.develop@outlook.com>
2026-09-10 20:02:15 +0800

    feat(0342): 4 的幂，解决问题，Kotlin 。

--- src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0342/Solution0342.kt
+++ src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0342/Solution0342.kt
@@ -1,8 +1,26 @@
 package com.github.xumeng.kotlin.problems.easy.problem0342
 
+import kotlin.math.pow
+
 class Solution0342 {
 
     fun isPowerOfFour(n: Int): Boolean {
+        if (n < 1) {
+            return false
+        }
+        var powerOfFour = 0.0
+        var i = 0
+        val base = 4.0
+        while (powerOfFour < Int.MAX_VALUE) {
+            powerOfFour = base.pow(i)
+            if (n.toDouble() == powerOfFour) {
+                return true
+            }
+            if (n < powerOfFour) {
+                return false
+            }
+            i++
+        }
         return false
     }
 
