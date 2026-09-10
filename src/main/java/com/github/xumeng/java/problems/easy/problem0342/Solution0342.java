f2eb433f14ecde6b7b87edbcbb3d8c3258e7ff12
xumeng <xumeng.develop@outlook.com>
2026-09-10 19:41:05 +0800

    feat(0342): 4 的幂，解决问题，Java。

--- src/main/java/com/github/xumeng/java/problems/easy/problem0342/Solution0342.java
+++ src/main/java/com/github/xumeng/java/problems/easy/problem0342/Solution0342.java
@@ -3,6 +3,19 @@ package com.github.xumeng.java.problems.easy.problem0342;
 public class Solution0342 {
 
     public boolean isPowerOfFour(int n) {
+        if (n < 1) {
+            return false;
+        }
+        double powerOfFour = 0.0d;
+        for (int i = 0; powerOfFour <= Integer.MAX_VALUE; i++) {
+            powerOfFour = Math.pow(4, i);
+            if (n == powerOfFour) {
+                return true;
+            }
+            if (n < powerOfFour) {
+                return false;
+            }
+        }
         return false;
     }
 
