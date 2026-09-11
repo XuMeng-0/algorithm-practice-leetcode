f3a7606117b0236f71b696eead14118c1d6253cb
xumeng <xumeng.develop@outlook.com>
2026-09-11 11:41:55 +0800

    feat(0062): 不同路径，解决问题，Java 。

--- src/main/java/com/github/xumeng/java/problems/medium/problem0062/Solution0062.java
+++ src/main/java/com/github/xumeng/java/problems/medium/problem0062/Solution0062.java
@@ -3,7 +3,16 @@ package com.github.xumeng.java.problems.medium.problem0062;
 public class Solution0062 {
 
     public int uniquePaths(int m, int n) {
-        return 1;
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+        if (m == 2) {
+            return n;
+        }
+        if (n == 2) {
+            return m;
+        }
+        return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
     }
 
 }
