1d961923941205ff37553660ae80387bc6fede59
xumeng <xumeng.develop@outlook.com>
2026-09-17 12:39:46 +0800

    feat(0344): 反转字符串，解决问题，Java。

--- src/main/java/com/github/xumeng/java/problems/easy/problem0344/Solution0344.java
+++ src/main/java/com/github/xumeng/java/problems/easy/problem0344/Solution0344.java
@@ -3,7 +3,15 @@ package com.github.xumeng.java.problems.easy.problem0344;
 public class Solution0344 {
 
     public void reverseString(char[] s) {
-
+        if (s.length == 1) {
+            return;
+        }
+        char temp;
+        for (int i = 0, j = s.length - 1; i < j; i++, j--) {
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }
     }
 
 }
