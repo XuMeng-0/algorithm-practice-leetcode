05b398a38ebb377d034643aaa01f0d298994cda6
xumeng <xumeng.develop@outlook.com>
2026-09-17 12:01:57 +0800

    feat(0344): 反转字符串，解决问题，Kotlin 。

--- src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0344/Solution0344.kt
+++ src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0344/Solution0344.kt
@@ -2,8 +2,20 @@ package com.github.xumeng.kotlin.problems.easy.problem0344
 
 class Solution0344 {
 
-    fun reverseString(s: CharArray): Unit {
-
+    fun reverseString(s: CharArray) {
+        if (s.size == 1) {
+            return
+        }
+        var i = 0
+        var j = s.size - 1
+        var temp: Char
+        while (i < j) {
+            temp = s[i]
+            s[i] = s[j]
+            s[j] = temp
+            i++
+            j--
+        }
     }
 
 }
