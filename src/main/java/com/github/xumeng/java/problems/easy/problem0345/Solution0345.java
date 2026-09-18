72a4cbdfb2a133e1176816dab43dddbfe115faf5
xumeng <xumeng.develop@outlook.com>
2026-09-18 10:53:47 +0800

    feat(0345): 反转字符串中的元音字母，解决问题，Java 。

--- src/main/java/com/github/xumeng/java/problems/easy/problem0345/Solution0345.java
+++ src/main/java/com/github/xumeng/java/problems/easy/problem0345/Solution0345.java
@@ -3,7 +3,35 @@ package com.github.xumeng.java.problems.easy.problem0345;
 public class Solution0345 {
 
     public String reverseVowels(String s) {
-        return s;
+        char[] characters = s.toCharArray();
+        char temp;
+        for (int i = 0, j = characters.length - 1; i < j; ) {
+            if (isNotVowel(characters[i])) {
+                i++;
+                continue;
+            }
+            if (isNotVowel(characters[j])) {
+                j--;
+                continue;
+            }
+            temp = characters[i];
+            characters[i] = characters[j];
+            characters[j] = temp;
+            i++;
+            j--;
+        }
+        StringBuilder builder = new StringBuilder();
+        for (char character : characters) {
+            builder.append(character);
+        }
+        return builder.toString();
+    }
+
+    private boolean isNotVowel(char character) {
+        return switch (character) {
+            case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' -> false;
+            default -> true;
+        };
     }
 
 }
