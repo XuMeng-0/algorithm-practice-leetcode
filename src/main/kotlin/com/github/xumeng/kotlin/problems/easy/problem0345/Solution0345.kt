4a3c5198393fd0b97d6e4cd8cbced20965a19c68
xumeng <xumeng.develop@outlook.com>
2026-09-18 11:44:18 +0800

    feat(0345): 反转字符串中的元音字母，解决问题，Kotlin 。

--- src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0345/Solution0345.kt
+++ src/main/kotlin/com/github/xumeng/kotlin/problems/easy/problem0345/Solution0345.kt
@@ -3,7 +3,33 @@ package com.github.xumeng.kotlin.problems.easy.problem0345
 class Solution0345 {
 
     fun reverseVowels(s: String): String {
-        return s
+        val characters = s.toCharArray()
+        var temp: Char
+        var i = 0
+        var j = characters.size - 1
+        while (i < j) {
+            if (isNotVowel(characters[i])) {
+                i++
+                continue
+            }
+            if (isNotVowel(characters[j])) {
+                j--
+                continue
+            }
+            temp = characters[i]
+            characters[i] = characters[j]
+            characters[j] = temp
+            i++
+            j--
+        }
+        return String(characters)
+    }
+
+    private fun isNotVowel(character: Char): Boolean {
+        return when (character) {
+            'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' -> false
+            else -> true
+        }
     }
 
 }
